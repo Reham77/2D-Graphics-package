18b5135d99d0d5dc9303b62e6e06dc0dee4cfafd
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-28 07:37:57 +0200

    added direct cartesian algorithm to draw circles

--- Circle.cpp
+++ Circle.cpp
@@ -20,4 +20,16 @@ void Circle::drawEightPoints(Point center, Point point) {
     SetPixel(hdc, center.x + point.y, center.y - point.x, color);
 }
 
-/
+//x^2 + y^2 = R^2
+void Circle::DirectCartesian(Point center, Point point) {
+    int radius = getRadius(center, point);
+    int x = radius;
+    int y = 0;
+    drawEightPoints(center, Point(x, y));
+    while (y < x) {
+        y++;
+        x = Round(sqrt(radius * radius - y * y));
+        drawEightPoints(center, Point(x, y));
+    }
+}
+
