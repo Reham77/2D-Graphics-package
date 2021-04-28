86ec4cb0c1ee622099328389ead6be283e4c1f26
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-28 07:05:28 +0200

    Added a draw line function using the parametric equation

--- Line.cpp
+++ Line.cpp
@@ -5,3 +5,16 @@
 Line::Line(HDC hdc, int ID, COLORREF color) :Shape(hdc, ID, color) {
 
 }
+//using parametric equation
+void Line::parametricLine(Point startPoint, Point endPoint) {
+    double dx = (double) endPoint.x - startPoint.x;
+    double dy = (double) endPoint.y - startPoint.y;
+
+    double dt = 1.00 / max(abs(dy), abs(dx));
+    for (double t = 0; t <= 1; t += dt) {
+        int x = Round(startPoint.x + (t * dx));
+        int y = Round(startPoint.y + (t * dy));
+
+        SetPixel(hdc , x , y , color);
+    }
+}
