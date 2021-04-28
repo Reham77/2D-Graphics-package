5357e7f32c3ab5bbfbbb6f855a571f76031f1dc4
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-28 08:18:40 +0200

    added header file for clipping class

---
+++ Clipping.h
@@ -0,0 +1,37 @@
+#include "Shape.h"
+#include "Line.h"
+
+union OutCode {
+    unsigned All: 4;
+    struct {
+        unsigned left: 1, top: 1, right: 1, bottom: 1;
+    };
+};
+
+
+class Clipping : public Shape {
+private:
+
+    OutCode GetOutCode(double x , double y , vector<Point>clippingWindow);
+    void X_Intersect(double Xstart, double Ystart, double Xend, double Yend, int x, double *xi, double *yi);
+    void Y_Intersect(double Xstart, double Ystart, double Xend, double Yend, int y, double *xi, double *yi);
+    void drawClippedLine_CohenSuth(Point start, Point end, vector<Point> clippingWindow);
+
+
+    void drawClippedPoint(Point point, vector<Point> clippingWindow);
+
+    void drawClippedCircle(Point center, Point point, vector<Point> clippingWindow);
+
+    bool clipped(Point point , vector<Point>clippingWindow );
+
+public:
+
+    Clipping(HDC hdc, int ID, COLORREF color);
+
+    vector<Point> drawClippingWindow(Point topLeft, Point bottomRight);
+
+    void draw(Point start, Point end, vector<Point> clippingWindow);
+
+
+};
+
