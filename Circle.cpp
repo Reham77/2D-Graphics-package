d45a0568d5f088a3280d9c62dd701948b9e8d186
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-28 07:34:44 +0200

    added a function to get radius of a circle

--- Circle.cpp
+++ Circle.cpp
@@ -3,3 +3,8 @@
 Circle::Circle(HDC hdc, int ID, COLORREF color) : Shape(hdc, ID, color) {
 
 }
+
+int Circle::getRadius(Point center, Point point) {
+    return hypot(point.x - center.x, point.y - center.y);
+}
+
