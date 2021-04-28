8641ba9e45c30224a5626084ac13a6a1bcdbd9a0
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-28 08:32:38 +0200

    added function to clip a circle to a rectangle window

--- Clipping/Clipping.cpp
+++ Clipping/Clipping.cpp
@@ -34,3 +34,30 @@ bool Clipping::clipped(Point point, vector<Point> clippingWindowPoints) {
             point.y <= clippingWindowPoints[1].y);
 
 }
+
+void Clipping::drawClippedPoint(Point point, vector<Point> clippingWindow) {
+
+    if (clipped(point, clippingWindow))
+        SetPixel(hdc, point.x, point.y, color);
+}
+
+void Clipping::drawClippedCircle(Point center, Point point, vector<Point> clippingWindow) {
+
+    int radius = hypot(center.x - point.x, center.y - point.y);
+    double dtheta = 1.0 / radius;
+    double ctheta = cos(dtheta);
+    double stheta = sin(dtheta);
+    double x = radius;
+    double y = 0.0;
+    drawClippedPoint(Point(x, y), clippingWindow);
+
+    for (double theta = 0; theta <= 6.28; theta += dtheta) {
+        double x1 = x * ctheta - y * stheta;
+        y = x * stheta + y * ctheta;
+        x = x1;
+        if (clipped(point, clippingWindow)) {
+            drawClippedPoint(Point(Round(x + center.x), Round(y + center.y)), clippingWindow);
+        }
+    }
+}
+
