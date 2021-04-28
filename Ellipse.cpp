609c508cf38445d3a65b978a0f4397ab340ed6db
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-28 07:19:19 +0200

    Added a function to draw Ellipse using polar equation

--- Ellipse.cpp
+++ Ellipse.cpp
@@ -8,3 +8,27 @@ Ellipse::Ellipse(HDC hdc, int ID, COLORREF color) :
 int Ellipse::getRadius(Point center, Point point) {
     return hypot(center.x - point.x, center.y - point.y);
 }
+
+void Ellipse::drawFourPoints(Point center, Point point) {
+
+    SetPixel(hdc, center.x + point.x, center.y + point.y, color);
+    SetPixel(hdc, center.x - point.x, center.y + point.y, color);
+    SetPixel(hdc, center.x - point.x, center.y - point.y, color);
+    SetPixel(hdc, center.x + point.x, center.y - point.y, color);
+
+}
+
+void Ellipse::Polar(Point center, Point a, Point b) {
+
+    int RadiusA = getRadius(center, a);
+    int RadiusB = getRadius(center, b);
+    double dtheta = 1.0 / max(RadiusA, RadiusB);
+
+    for (double theta = 0; theta <= 6.28; theta += dtheta) {
+        int x = Round(RadiusA * cos(theta));
+        int y = Round(RadiusB * sin(theta));
+        SetPixel(hdc, center.x + x, center.y + y, color);
+    }
+}
+
+
