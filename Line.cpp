7e1a75802b09fce351f23c88e48f0fee93c7efd0
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-28 07:07:13 +0200

    Added a line drawing function using DDA algorithm

--- Line.cpp
+++ Line.cpp
@@ -18,3 +18,42 @@ void Line::parametricLine(Point startPoint, Point endPoint) {
         SetPixel(hdc , x , y , color);
     }
 }
+
+// y = mx + c
+void Line::DDA(Point startPoint, Point endPoint) {
+    double dx = endPoint.x - startPoint.x;
+    double dy = endPoint.y - startPoint.y;
+    double m = 0;
+    if (abs(dy) <= abs(dx)) // if slope is less than or equal to one
+    {
+        m = dy / dx;
+        if (startPoint.x > endPoint.x) {
+            swap(startPoint.x, endPoint.x);
+            swap(startPoint.y, endPoint.y);
+        }
+        int x = startPoint.x;
+        double y = startPoint.y;
+        SetPixel(hdc , x , Round(y) , color);
+        while (x < endPoint.x) {
+            x++;
+            y += m;
+            SetPixel(hdc , x , Round(y) , color);
+        }
+    } else {
+        m = dx / dy;
+        if (startPoint.y > endPoint.y) {
+            swap(startPoint.y, endPoint.y);
+            swap(startPoint.x, endPoint.x);
+        }
+        double x = startPoint.x;
+        int y = startPoint.y;
+        while (y < endPoint.y) {
+            y++;
+            x += m;
+            SetPixel(hdc , Round(x) , y , color);
+        }
+    }
+}
+
+
+
