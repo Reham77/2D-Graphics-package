bde8181612cccbfb3d8e54282d5cf599cb6ce647
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-28 07:20:15 +0200

    added enhanced iterative polar algorithm

--- Ellipse.cpp
+++ Ellipse.cpp
@@ -31,4 +31,61 @@ void Ellipse::Polar(Point center, Point a, Point b) {
     }
 }
 
+void Ellipse::direct(Point center, Point a, Point b) {
+
+    int RadiusA = getRadius(center, a);
+    int RadiusB = getRadius(center, b);
+    double x = 0;
+    double y = RadiusB;
+    drawFourPoints(center, Point(x, y));
+    //check slope less than 1
+    while (RadiusB * RadiusB * x <= RadiusA * RadiusA * y) {
+        x++;
+        y = Round((sqrt(RadiusB * RadiusB * (1 - ((x * x) / (RadiusA * RadiusA))))));
+        drawFourPoints(center, Point(x, y));
+    }
+    x = RadiusA;
+    y = 0;
+    drawFourPoints(center, Point(x, y));
+    //slope greater than one
+    while (RadiusB * RadiusB * x > RadiusA * RadiusA * y) {
+        y++;
+        x = Round((sqrt(RadiusA * RadiusA * (1 - ((y * y) / (RadiusB * RadiusB))))));
+        drawFourPoints(center, Point(x, y));
+    }
+}
+
+//don't calculate sin and cos inside loop
+void Ellipse::iterativePolar(Point center, Point a, Point b) {
+
+    int RadiusA = getRadius(center, a);
+    int RadiusB = getRadius(center, b);
+
+    double dtheta = 1.0 / max(RadiusA, RadiusB);
+    double stheta = sin(dtheta);
+    double ctheta = cos(dtheta);
+    double x = center.x + RadiusA;
+    double y = center.y;
+    double x1 = (x - center.x) / RadiusA;
+    double y1 = (y - center.y) / RadiusB;
+    SetPixel(hdc, Round(x), Round(y), color);
+
+    for (double theta = 0; theta <= 6.28; theta += dtheta) {
+        x1 = (x - center.x) / RadiusA;
+        y1 = (y - center.y) / RadiusB;
+        x = x * ctheta - y1 * RadiusA * stheta;
+        y = y * ctheta + x1 * RadiusB * stheta;
+        SetPixel(hdc, Round(x), Round(y), color);
+    }
+}
+
+void Ellipse::draw(Point point, Point a, Point b) {
+
+    if (ID == ELLIPSE_POLLAR)
+        Polar(point, b, a);
+    else if (ID == ELLIPSE_ITERATIVE_POLLAR)
+        iterativePolar(point, b, a);
+    else direct(point, b, a);
+}
+
 
