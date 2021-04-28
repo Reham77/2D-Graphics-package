7c685511f10abc2005a647d194893380b4dbe9f9
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-28 07:35:57 +0200

    added a function to draw 8 points

--- Circle.cpp
+++ Circle.cpp
@@ -8,3 +8,16 @@ int Circle::getRadius(Point center, Point point) {
     return hypot(point.x - center.x, point.y - center.y);
 }
 
+void Circle::drawEightPoints(Point center, Point point) {
+
+    SetPixel(hdc, center.x + point.x, center.y + point.y, color);
+    SetPixel(hdc, center.x - point.x, center.y + point.y, color);
+    SetPixel(hdc, center.x - point.x, center.y - point.y, color);
+    SetPixel(hdc, center.x + point.x, center.y - point.y, color);
+    SetPixel(hdc, center.x + point.y, center.y + point.x, color);
+    SetPixel(hdc, center.x - point.y, center.y + point.x, color);
+    SetPixel(hdc, center.x - point.y, center.y - point.x, color);
+    SetPixel(hdc, center.x + point.y, center.y - point.x, color);
+}
+
+/
